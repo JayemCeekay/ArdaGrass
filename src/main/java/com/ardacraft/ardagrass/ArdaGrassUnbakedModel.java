3a976820617ec4ada24bd30ce5fe6fc9de58e9c0
JayemCeekay <mrjmck95@gmail.com>
2023-09-08 12:40:38 -0500

    update to 1.20.1

--- src/main/java/com/ardacraft/ardagrass/ArdaGrassUnbakedModel.java
+++ src/main/java/com/ardacraft/ardagrass/ArdaGrassUnbakedModel.java
@@ -1,18 +1,15 @@
 package com.ardacraft.ardagrass;
 
-import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.Baker;
 import net.minecraft.client.render.model.ModelBakeSettings;
-import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Set;
 import java.util.function.Function;
 
 public class ArdaGrassUnbakedModel implements UnbakedModel {
@@ -22,20 +19,20 @@ public class ArdaGrassUnbakedModel implements UnbakedModel {
         this.baseModel = unbakedModel;
     }
 
+
     @Override
     public Collection<Identifier> getModelDependencies() {
         return this.baseModel.getModelDependencies();
     }
 
     @Override
-    public Collection<SpriteIdentifier> getTextureDependencies(Function<Identifier, UnbakedModel> unbakedModelGetter, Set<Pair<String, String>> unresolvedTextureReferences) {
-        var baseIds = this.baseModel.getTextureDependencies(unbakedModelGetter, unresolvedTextureReferences);
-        return new ArrayList<>(baseIds);
+    public void setParents(Function<Identifier, UnbakedModel> modelLoader) {
+        this.baseModel.setParents(modelLoader);
     }
 
     @Nullable
     @Override
-    public BakedModel bake(ModelLoader loader, Function<SpriteIdentifier, Sprite> textureGetter, ModelBakeSettings rotationContainer, Identifier modelId) {
+    public BakedModel bake(Baker loader, Function<SpriteIdentifier, Sprite> textureGetter, ModelBakeSettings rotationContainer, Identifier modelId) {
         return new ArdaGrassBakedModel(this.baseModel.bake(loader, textureGetter, rotationContainer, modelId));
     }
 }
